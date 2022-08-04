2add2574fb2ed3f84668997836c57afcb6eb962b
Anthony Collier <hello@acollier.dev>
2022-08-04 01:13:05 -0400

    :construction: Build out errors page with UI components

---
+++ src/components/Embeds/ErrorEmbed.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  DiscordMessages,
+  DiscordMessage,
+  DiscordEmbed,
+  DiscordEmbedDescription,
+  DiscordEmbedFooter,
+} from '@skyra/discord-components-react';
+import { EmbedColor, ErrorEmbedProps } from '../../constants';
+
+export default function ErrorEmbed({
+  title,
+  description,
+  code,
+}: ErrorEmbedProps): JSX.Element {
+  return (
+    <div className='padding-bottom--md'>
+      <DiscordMessages>
+        <DiscordMessage profile='suggestions' ephemeral>
+          <DiscordEmbed slot='embeds' color={EmbedColor.ERROR} title={title}>
+            <DiscordEmbedDescription slot='description'>
+              {description}
+            </DiscordEmbedDescription>
+            <DiscordEmbedFooter slot='footer' timestamp={new Date()}>
+              Error code {code} | Cluster 1
+            </DiscordEmbedFooter>
+          </DiscordEmbed>
+        </DiscordMessage>
+      </DiscordMessages>
+    </div>
+  );
+}
--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,22 @@ export enum EmbedColor {
   MAIN = '#ffd663',
   APPROVED = '#00e640',
   REJECTED = '#cf000f',
+  ERROR = '#d63031',
+}
+
+export enum ErrorCode {
+  SUGGESTION_MESSAGE_DELETED = 1,
+  MISSING_PERMISSIONS = 2,
+  MISSING_SUGGESTIONS_CHANNEL = 3,
+  MISSING_LOG_CHANNEL = 4,
+  SUGGESTION_NOT_FOUND = 5,
+  OWNER_ONLY = 6,
+  SUGGESTION_CONTENT_TOO_LONG = 7,
+  INVALID_GUILD_CONFIG_CHOICE = 8,
+  COMMAND_ON_COOLDOWN = 9,
+  GENERIC_FORBIDDEN = 10,
+  UNHANDLED_ERROR = 11,
+  GENERIC_NOT_FOUND = 12,
 }
 
 export type EmbedProps = {
@@ -11,4 +27,10 @@ export type EmbedProps = {
   thumbnail?: boolean;
   displayEmbedAuthor?: boolean;
   ephemeral?: boolean;
+};
+
+export type ErrorEmbedProps = {
+  title: string;
+  description: string;
+  code: ErrorCode;
 };
